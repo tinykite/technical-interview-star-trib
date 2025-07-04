c90cbe4f98cc0f8f2db25100237aca32b52ce02f
tinykite <dakota@tinykitelab.com>
2025-07-04 09:39:52 -0500

    feat: Update article views to indicate a favorited/followed author

--- apps/web/src/northern-star/ArticlePreview/ArticlePreview.tsx
+++ apps/web/src/northern-star/ArticlePreview/ArticlePreview.tsx
@@ -1,6 +1,8 @@
 'use client';
 
 import Link from 'next/link';
+import { useFavorites } from '@/contexts/FavoritesContext';
+import Pill from '../Pill/Pill';
 
 export type Article = {
   title: string;
@@ -12,10 +14,13 @@ export type Article = {
 };
 
 export default function ArticlePreview({ article }: { article: Article }) {
+    const { isFavorite } = useFavorites()
+    const favorited = isFavorite(article.author)
+
   return (
     <Link
       href={`/article/${article.slug}`}
-      className="block bg-white shadow-md rounded-md overflow-hidden hover:shadow-lg transition"
+      className="block bg-white shadow-md rounded-md overflow-hidden hover:shadow-lg transition relative"
     >
       {article.image && (
         <img
@@ -25,9 +30,19 @@ export default function ArticlePreview({ article }: { article: Article }) {
         />
       )}
       <div className="p-4">
-        <h2 className="text-xl font-semibold">{article.title}</h2>
-        <p className="text-gray-700">{article.summary}</p>
+      
+        <h2 className="text-xl font-semibold">{article.title}</h2> 
+        <div className="flex space-x-2">  
+        <p>{article.author}</p>
+         {favorited && <Pill label="following"/>}
+        </div>
+
+        <p className="text-gray-900">{article.summary}</p>
+
+      
       </div>
+
+    
     </Link>
   );
 }
